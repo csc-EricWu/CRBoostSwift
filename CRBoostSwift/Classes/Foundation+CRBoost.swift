cd7d190bff2b1fa3da5ee4400f80a9a374467ef3
Eric <starring.wu@gmail.com>
2019-12-24 14:56:56 +0800

    增加颜色获取扩展

--- CRBoostSwift/Classes/Foundation+CRBoost.swift
+++ CRBoostSwift/Classes/Foundation+CRBoost.swift
@@ -328,25 +328,25 @@ extension StringProtocol {
     public subscript(_ offset: Int) -> Element {
         self[index(startIndex, offsetBy: offset)]
     }
-    
+
     public subscript(_ range: Range<Int>) -> SubSequence {
-        prefix(range.lowerBound+range.count).suffix(range.count)
+        prefix(range.lowerBound + range.count).suffix(range.count)
     }
-    
+
     public subscript(_ range: ClosedRange<Int>) -> SubSequence {
-        prefix(range.lowerBound+range.count).suffix(range.count)
+        prefix(range.lowerBound + range.count).suffix(range.count)
     }
-    
+
     public subscript(_ range: PartialRangeThrough<Int>) -> SubSequence {
         prefix(range.upperBound.advanced(by: 1))
     }
-    
+
     public subscript(_ range: PartialRangeUpTo<Int>) -> SubSequence {
         prefix(range.upperBound)
     }
-    
+
     public subscript(_ range: PartialRangeFrom<Int>) -> SubSequence {
-        suffix(Swift.max(0, count-range.lowerBound))
+        suffix(Swift.max(0, count - range.lowerBound))
     }
 }
 
@@ -360,7 +360,6 @@ extension BidirectionalCollection {
         return self[i]
     }
 }
-
 
 extension URL {
     @discardableResult
@@ -751,6 +750,8 @@ extension UIImage {
 }
 
 extension UIColor {
+    @discardableResult
+
     public class func randomColor() -> UIColor {
         let red = arc4random_uniform(256)
         let green = arc4random_uniform(256)
@@ -758,9 +759,63 @@ extension UIColor {
         return CRRGBA(r: Float(red), g: Float(green), b: Float(blue))
     }
 
-    // MARK: - color from {R, G, B}
+    @discardableResult
+    public func hexString(_ alpha: Bool = false) -> String {
+        let comps = cgColor.components!
+        let r = Int(comps[0] * 255)
+        let g = Int(comps[1] * 255)
+        let b = Int(comps[2] * 255)
+        let a = Int(comps[3] * 255)
+        var hexString: String = "#"
 
-    public class func colorWithString(string: String) -> UIColor {
-        return UIColor()
+        hexString += String(format: "%02X%02X%02X", r, g, b)
+        if alpha {
+            hexString += String(format: "%02X", a)
+        }
+        return hexString
+    }
+
+    @discardableResult
+    public class func colorWithHex(hex: String, _ alpha: CGFloat = 1) -> UIColor? {
+        var hexString = hex
+        if hex.hasPrefix("#") {
+            let index = hex.index(hex.startIndex, offsetBy: 1)
+            hexString = String(hex[index...])
+        }
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha = alpha
+        let scanner = Scanner(string: hexString)
+        var hexValue: CUnsignedLongLong = 0
+        if scanner.scanHexInt64(&hexValue) {
+            switch hexString.count {
+            case 3:
+                red = CGFloat((hexValue & 0xF00) >> 8) / 15.0
+                green = CGFloat((hexValue & 0x0F0) >> 4) / 15.0
+                blue = CGFloat(hexValue & 0x00F) / 15.0
+            case 4:
+                red = CGFloat((hexValue & 0xF000) >> 12) / 15.0
+                green = CGFloat((hexValue & 0x0F00) >> 8) / 15.0
+                blue = CGFloat((hexValue & 0x00F0) >> 4) / 15.0
+                alpha = CGFloat(hexValue & 0x000F) / 15.0
+            case 6:
+                red = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+                green = CGFloat((hexValue & 0x00FF00) >> 8) / 255.0
+                blue = CGFloat(hexValue & 0x0000FF) / 255.0
+            case 8:
+                red = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                blue = CGFloat((hexValue & 0x0000FF00) >> 8) / 255.0
+                alpha = CGFloat(hexValue & 0x000000FF) / 255.0
+            default:
+                // Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8
+                return nil
+            }
+        } else {
+            // "Scan hex error
+            return nil
+        }
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
